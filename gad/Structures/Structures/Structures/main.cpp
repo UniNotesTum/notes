3e723784ff9ca614876a1284ab2d7b9664dce8e1
yuriyarabskyy <yuriy.arabskyy@gmail.com>
2016-05-19 22:46:50 +0200

    grspr

---
+++ gad/Structures/Structures/Structures/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Structures
+//
+//  Created by Yuriy Arabskyy on 10/05/16.
+//  Copyright © 2016 Yuriy Arabskyy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
